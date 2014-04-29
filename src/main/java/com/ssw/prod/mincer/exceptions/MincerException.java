28473cadb5c95b45ab4e19a11338106f9fed7063
Marco Genova <markgen@gmail.com>
2014-04-29 15:19:38 +0200

    Created base taxonomy exception

---
+++ src/main/java/com/ssw/prod/mincer/exceptions/MincerException.java
@@ -0,0 +1,42 @@
+package com.ssw.prod.mincer.exceptions;
+
+/**
+ *
+ * @author m.genova
+ * @since 0.0.1
+ */
+public class MincerException extends Exception {
+    
+    protected int code;
+    
+    public MincerException() {
+        super();
+        code = -1;
+    }
+    
+    public MincerException(int errorCode) {
+        super();
+        code = errorCode;
+    }
+    
+    public MincerException(int errorCode, String message) {
+        super(message);
+        code = errorCode;
+    }
+    
+    public MincerException(int errorCode, String message, Throwable innerException) {
+        super(message, innerException);
+        code = errorCode;
+    }
+    
+    public MincerException(String message) {
+        super(message);
+        code = -1;
+    }
+    
+    public MincerException(String message, Throwable innerException) {
+        super(message, innerException);
+        code = -1;
+    }
+    
+}
